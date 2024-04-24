218893c0e6899774cf96db042357622d058c295f
Nipuni Sathsarani <79597637+nipuni-sathsarani@users.noreply.github.com>
2024-04-24 17:45:28 +0530

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,3 +1,6 @@
 test('#1', function () {
     assert.isNull(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
   });
+  test('#1', function () {
+    assert.isNotNull(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
+  });  
