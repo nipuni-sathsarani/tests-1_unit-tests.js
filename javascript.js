90a757e4a73f47fdd7bb4434e41c8ee7c0e102d0
Nipuni Sathsarani <79597637+nipuni-sathsarani@users.noreply.github.com>
2024-04-24 17:51:28 +0530

    Add files via upload

--- javascript.js
+++ javascript.js
@@ -1,6 +1,10 @@
-test('#1', function () {
-    assert.isNull(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
+const assert = require('chai').assert;
+
+suite('Unit Tests', function(){
+  
+  // Test #1
+  // Change assert to assert.isNull or assert.isNotNull
+  test('#1 - #isNull, #isNotNull', function(){
+    assert.isNotNull('null', 'this is an optional message');
   });
-  test('#1', function () {
-    assert.isNotNull(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
-  });  
+});
