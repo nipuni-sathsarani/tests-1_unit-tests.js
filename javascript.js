1ae6d9b193d87bb2d940054c78ea9e5dd6a8997d
Nipuni Sathsarani <79597637+nipuni-sathsarani@users.noreply.github.com>
2024-04-24 17:52:48 +0530

    Update javascript.js

--- javascript.js
+++ javascript.js
@@ -1,10 +1,8 @@
-const assert = require('chai').assert;
-
-suite('Unit Tests', function(){
-  
-  // Test #1
-  // Change assert to assert.isNull or assert.isNotNull
-  test('#1 - #isNull, #isNotNull', function(){
-    assert.isNotNull('null', 'this is an optional message');
-  });
+/** 1 - Use assert.isNull() or assert.isNotNull() to make the tests pass. **/
+test('#isNull, #isNotNull', function() {
+  assert.isNull(
+    null,
+    'this is an optional error description - e.g. null is null'
+  );
+  assert.isNotNull(1, '1 is not null');
 });
